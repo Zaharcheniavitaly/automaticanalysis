d75e69ca2d0dc63f7d8d1051834f40989edbe328
Zaharcheniavitaly <98647075+Zaharcheniavitaly@users.noreply.github.com>
2023-03-28 18:54:25 +0300

    faq-pages

--- js/main.js
+++ js/main.js
@@ -1,31 +1,27 @@
-// Burger
-const nav = document.querySelector("#nav");
-const navBtn = document.querySelector("#nav-btn");
-const navBtnImg = document.querySelector("#nav-btn-img");
-
-const textEl = document.querySelectorAll('.text')
-const buttonText = document.querySelector('.button-text')
+// -------------------------- Burger ----------------
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
 
 function handlerBurger() {
-  if (nav.classList.toggle("open")) {
-    navBtnImg.src = "./img/icon/close-nav.svg";
+  if (nav.classList.toggle('open')) {
+    navBtnImg.src = './img/icon/close-nav.svg';
   } else {
-    navBtnImg.src = "./img/icon/burger-bl.png";
+    navBtnImg.src = './img/icon/burger-bl.png';
   }
 }
+navBtn.addEventListener('click', handlerBurger);
 
-navBtn.addEventListener("click", handlerBurger);
+// ---------------- Появляющийся текст на главной стр. -----------------
 
+const textEl = document.querySelectorAll('.text');
+const buttonText = document.querySelector('.button-text');
 
-
-
-function clickHandler (){
-  textEl.forEach((item)=>{
-  item.classList.remove("hidden")
-  buttonText.classList.add("hidden")
-})
+function clickHandler() {
+  textEl.forEach((item) => {
+    item.classList.remove('hidden');
+    buttonText.classList.add('hidden');
+  });
 }
 
-buttonText.addEventListener('click', clickHandler)
-
-
+buttonText.addEventListener('click', clickHandler);
