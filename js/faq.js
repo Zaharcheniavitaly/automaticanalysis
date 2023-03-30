b440eadf1e0113d536b23e7689458863ae6928a5
Zaharcheniavitaly <98647075+Zaharcheniavitaly@users.noreply.github.com>
2023-03-30 21:46:34 +0300

    question-answer

--- js/faq.js
+++ js/faq.js
@@ -2,41 +2,28 @@
 
 const questionsBtns = document.querySelectorAll('.question-border');
 const answers = document.querySelectorAll(' .question-answer');
-// console.log("answers:", answers)
-const arrowButtom = document.querySelectorAll('.questions-arrow-buttom');
-const arrowTop = document.querySelectorAll('.questions-arrow-top');
-console.log("arrowTop:", arrowTop)
+console.log('answers:', answers);
+const arrowButtom = document.querySelectorAll('.arrow');
+console.log('arrowButtom:', arrowButtom);
+// const arrowTop = document.querySelectorAll('.questions-arrow-top');
+// console.log('arrowTop:', arrowTop);
 
 function textHiddenButtom(e) {
-  answers.forEach((answ) => {
-    console.log('Вверх');
+  answers.forEach((answ, index) => {
+    const answerID = e.target.id;
+    console.log('answers.forEach > answerID:', answerID);
 
-    if (e.target.classList.contains('questions-arrow-buttom')) {
-
-      e.target.classList.remove('questions-arrow-buttom');
-      e.target.classList.add('questions-arrow-top');
+    if (answerID == index) {
+      if (e.target.classList.contains('questions-arrow-buttom')) {
+        e.target.classList.toggle('questions-arrow-top');
+        answers[answerID].classList.toggle('hidden');
+      }
+    } else {
+      answ.classList.add('hidden');
     }
-
-   
-
-    answ.classList.remove('hidden');
   });
 }
 
 arrowButtom.forEach((item) => {
   item.addEventListener('click', textHiddenButtom);
-  console.log('itemButtom', item);
-});
-
-function textHiddenTop(e){
-    console.log('Вниз');
-if (e.target.classList.contains('questions-arrow-top')) {
-      e.target.classList.remove('questions-arrow-top');
-      e.target.classList.add('questions-arrow-buttom');
-    }
-}
-
-
-arrowTop.forEach((item) => {
-  item.addEventListener('click', textHiddenTop);
 });
