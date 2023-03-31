956b7078ef15623a36d9afd268471c38c4b27ad6
Zaharcheniavitaly <98647075+Zaharcheniavitaly@users.noreply.github.com>
2023-03-31 16:37:01 +0300

    legkost img

--- js/faq.js
+++ js/faq.js
@@ -2,16 +2,16 @@
 
 const questionsBtns = document.querySelectorAll('.question-border');
 const answers = document.querySelectorAll(' .question-answer');
-console.log('answers:', answers);
+// console.log('answers:', answers);
 const arrowButtom = document.querySelectorAll('.arrow');
-console.log('arrowButtom:', arrowButtom);
+// console.log('arrowButtom:', arrowButtom);
 // const arrowTop = document.querySelectorAll('.questions-arrow-top');
 // console.log('arrowTop:', arrowTop);
 
 function textHiddenButtom(e) {
   answers.forEach((answ, index) => {
     const answerID = e.target.id;
-    console.log('answers.forEach > answerID:', answerID);
+    // console.log('answers.forEach > answerID:', answerID);
 
     if (answerID == index) {
       if (e.target.classList.contains('questions-arrow-buttom')) {
@@ -20,6 +20,18 @@ function textHiddenButtom(e) {
       }
     } else {
       answ.classList.add('hidden');
+
+      arrowButtom.forEach((item) => {
+        if (answerID != index) {
+            console.log('answerID', answerID);
+            console.log('index', index);
+          if (item.classList.contains('questions-arrow-top')) {
+            item.classList.remove('questions-arrow-top');
+
+            console.log('classList', item.classList);
+          }
+        }
+      });
     }
   });
 }
@@ -27,3 +39,12 @@ function textHiddenButtom(e) {
 arrowButtom.forEach((item) => {
   item.addEventListener('click', textHiddenButtom);
 });
+
+// if(answerID !== index) {
+//     arrowButtom.forEach((item) => {
+//       if (item.classList.contains('questions-arrow-top')) {
+//         item.classList.remove('questions-arrow-top');
+//         console.log('arrowButtom.classList', item.classList);
+//       }
+//     });
+//   }
